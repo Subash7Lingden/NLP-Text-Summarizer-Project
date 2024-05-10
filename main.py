dff80899b13f8ccd04db4bb148a4ca42e65a5949
Subash7Lingden <subashsubbalingden@gmail.com>
2024-05-10 23:02:51 +0545

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from textSummarizer.pipeline .stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline .stage_02_data_validation import DataValidationTrainingPipeline
 #from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -16,15 +16,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Data Validation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_validation = DataValidationTrainingPipeline()
-#    data_validation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 # STAGE_NAME= "Data Transformation Stage"
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -9,11 +9,11 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
-# @dataclass(frozen=True)
-# class DataValidationConfig:
-#     root_dir: Path
-#     STATUS_FILE: str
-#     ALL_REQUIRED_FILES: list
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
 
 
 # @dataclass(frozen=True)
